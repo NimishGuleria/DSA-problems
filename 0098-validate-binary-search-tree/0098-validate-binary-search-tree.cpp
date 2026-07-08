6d2ff980c91e5d3be37d56ead0bed453feb82755
Nimish Guleria <152334144+NimishGuleria@users.noreply.github.com>
2026-07-08 14:55:52 +0530

    Time: 0 ms (100%), Space: 21.9 MB (44.84%) - LeetHub - Time: O(n), Space: O(h)

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    bool solve(TreeNode* root, long long minVal, long long maxVal) {
+
+        if(root == NULL)
+            return true;
+
+        if(root->val <= minVal || root->val >= maxVal)
+            return false;
+
+        return solve(root->left, minVal, root->val) &&
+               solve(root->right, root->val, maxVal);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        return solve(root, LLONG_MIN, LLONG_MAX);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
