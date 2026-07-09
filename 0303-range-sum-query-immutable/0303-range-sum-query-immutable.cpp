8a1c477fae1c9a9432cb5b51a853d424526ee056
Nimish Guleria <152334144+NimishGuleria@users.noreply.github.com>
2026-07-09 14:42:31 +0530

    Time: 0 ms (100%), Space: 23.8 MB (95.28%) - LeetHub - Time: O(n + q), Space: O(1)

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -0,0 +1,18 @@
+class NumArray { // 12 ms, faster than 99.87%
+public:
+    vector<int>& preSum; // `preSum` will reference to `nums` array, no copy at all!
+    
+    NumArray(vector<int>& nums) : preSum(nums) {
+        for (int i = 1; i < preSum.size(); ++i)
+            preSum[i] += preSum[i-1]; 
+    }
+    
+    int sumRange(int left, int right) {
+        if (left == 0) return preSum[right];
+        return preSum[right] - preSum[left-1];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
