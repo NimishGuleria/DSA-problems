e3a991bf67b21ee41ee4dab663d77043da9d6b66
Nimish Guleria <152334144+NimishGuleria@users.noreply.github.com>
2026-07-15 16:28:13 +0530

    graphs

--- 4271-find-the-degree-of-each-vertex/4271-find-the-degree-of-each-vertex.cpp
+++ 4271-find-the-degree-of-each-vertex/4271-find-the-degree-of-each-vertex.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-    static vector<int> findDegrees(vector<vector<int>>& matrix) {
-        const int n=matrix.size();
-        vector<int> deg(n, 0);
-        for(int i=0; i<n; i++) 
-            for(int j=0; j<i; j++){
-                bool is1=matrix[i][j];
-                deg[i]+=is1;
-                deg[j]+=is1;
+    vector<int> findDegrees(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<int> ans(n, 0);
+
+        for (int i = 0; i < n; i++) {
+            int degree = 0;
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 1) {
+                    degree++;
+                }
             }
-        return deg;
+            ans[i] = degree;
+        }
+        return ans;
     }
 };
 
