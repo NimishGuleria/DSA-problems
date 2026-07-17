6c5f6c040c82b0903e4628603c85d84be78b0248
Nimish Guleria <152334144+NimishGuleria@users.noreply.github.com>
2026-07-17 14:41:54 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 702382--GFG/702382-.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+void sort012(vector<int>& arr) {
+/* APPROACH - 1
+sort(arr.begin(), arr.end()); */
+ 
+int n = arr.size();
+int zeroes = 0;
+int ones = 0;
+for (int i = 0; i<n; i++) {if (arr[i] == 0) {zeroes++; }
+else if (arr[i] == 1) {ones++; }
+}
+for (int i = 0; i<zeroes; i++) {arr[i] = 0; }
+for (int i = zeroes; i<zeroes + ones; i++) {arr[i] = 1; }
+for (int i = zeroes + ones; i<n; i++) {arr[i] = 2; }
+ 
+}
+};
+ 
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
