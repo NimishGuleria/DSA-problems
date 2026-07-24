e6abfbb7ede1f2ca9284f301b7278145afcf85f4
Nimish Guleria <152334144+NimishGuleria@users.noreply.github.com>
2026-07-24 14:20:40 +0530

    Number of Unique XOR Triplets II   Time: 472 ms (78.09%), Space: 81.6 MB (49.44%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        const int M=2048;
+        vector<vector<int>> dp(4,vector<int>(M));
+        dp[0][0]=1;
+        for(int t=0;t<3;t++){
+            for(int x=0;x<M;x++){
+                if(!dp[t][x]) continue;
+                for(int v:nums) dp[t+1][x^v]=1;
+            }
+        }
+        int ans=0;
+        for(int x=0;x<M;x++){
+            if(dp[3][x]) ans++;
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
