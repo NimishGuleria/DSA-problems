b231eb07a4a06e96e568bb33532a22cd414cb229
Nimish Guleria <152334144+NimishGuleria@users.noreply.github.com>
2026-09-26 19:11:24 +0530

    Sliding Window Maximum

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> ans;
+        deque<int> dq;
+
+        for (int i = 0; i < n; i++) {
+            // Remove indices outside the window
+            if (!dq.empty() && dq.front() <= i - k) 
+                dq.pop_front();
+
+            // Maintain decreasing order in deque
+            while (!dq.empty() && nums[dq.back()] <= nums[i]) 
+                dq.pop_back();
+
+            dq.push_back(i);
+
+            // Add maximum for the current window
+            if (i >= k - 1) 
+                ans.push_back(nums[dq.front()]);
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
